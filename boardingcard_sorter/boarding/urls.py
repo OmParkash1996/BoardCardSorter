2545b33d250249e1344831695f9c923a4f31fbb9
OmParkash1996 <69729663+OmParkash1996@users.noreply.github.com>
2023-09-30 02:19:44 -0700

    Delete boardingcard_sorter directory

--- boardingcard_sorter/boarding/urls.py
+++
@@ -1,7 +0,0 @@
-# boarding/urls.py
-from django.urls import path
-from .views import BoardingCardSortView
-
-urlpatterns = [
-    path('boarding-cards/', BoardingCardSortView.as_view(), name='boarding-card-list'),
-]
